066a5b4050163ab686210717c14ac31a31858955
kanenorman <kanenorman1996@gmail.com>
2023-08-28 17:32:31 -0500

    Start flask dashboard

---
+++ app.py
@@ -0,0 +1,34 @@
+import psycopg2
+from flask import Flask, render_template
+
+from config import configs
+
+app = Flask(__name__)
+
+# Database connection parameters
+db_params = {
+    "dbname": configs.POSTGRES_DB,
+    "user": configs.POSTGRES_USER,
+    "password": configs.POSTGRES_PASSWORD,
+    "host": configs.POSTGRES_HOST,
+    "port": configs.POSTGRES_PORT,
+}
+
+
+@app.route("/")
+def index():
+    conn = psycopg2.connect(**db_params)
+    cursor = conn.cursor()
+
+    query = f"SELECT * FROM {configs.POSTGRES_TABLE}"
+    cursor.execute(query)
+    data = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return render_template("index.html", data=data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
