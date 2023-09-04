5825e1b36561cf2b3001a580463236739d7f003a
kanenorman <kanenorman1996@gmail.com>
2023-09-03 23:05:53 -0400

    Start docker kafka consumer

--- flask_app/app/views.py
+++ flask_app/app/views.py
@@ -1,8 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+
+from .models import Schedule
 
 main = Blueprint("main", __name__)
 
 
 @main.route("/")
 def index():
-    return "Hello World"
+    schedule_data = Schedule.query.all()
+    return render_template("index.html", data=schedule_data)
