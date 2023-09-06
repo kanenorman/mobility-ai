b68c2e95f16f41dd6aab56ae3daa0cf8b96d48a0
kanenorman <kanenorman1996@gmail.com>
2023-09-05 22:24:38 -0400

    Add multibroker support for producer and consumer

--- data_streaming/kafka_consumer/config.py
+++ data_streaming/kafka_consumer/config.py
@@ -4,8 +4,12 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class _Config:
-    KAFKA_HOST: str = os.environ["KAFKA_HOST"]
-    KAFKA_PORT: str = os.environ["KAFKA_PORT"]
+    KAFKA_HOST1: str = os.environ["KAFKA_HOST1"]
+    KAFKA_PORT1: str = os.environ["KAFKA_PORT1"]
+    KAFKA_HOST2: str = os.environ["KAFKA_HOST2"]
+    KAFKA_PORT2: str = os.environ["KAFKA_PORT2"]
+    KAFKA_HOST3: str = os.environ["KAFKA_HOST3"]
+    KAFKA_PORT3: str = os.environ["KAFKA_PORT3"]
     SCHEDULES_INPUT_TOPIC: str = os.environ["SCHEDULES_INPUT_TOPIC"]
     POSTGRES_PORT: str = os.environ["POSTGRES_PORT"]
     POSTGRES_DB: str = os.environ["POSTGRES_DB"]
