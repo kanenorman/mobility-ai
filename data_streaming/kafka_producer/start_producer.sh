754084b6353f8fcf6bc4654501c9a20d1579ef12
kanenorman <kanenorman1996@gmail.com>
2023-09-05 22:12:46 -0400

    Refactor start_producer.sh for multiple brokers
    Update script so that multiple brokers can be handled

--- data_streaming/kafka_producer/start_producer.sh
+++ data_streaming/kafka_producer/start_producer.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
+KAFKA_PORTS=("$KAFKA_PORT1" "$KAFKA_PORT2" "$KAFKA_PORT3")
+KAFKA_HOST=("$KAFKA_HOST1","$KAFKA_HOST2", "$KAFKA_HOST3")
+
 kafka_running() {
-    nc -z -w 5 kafka $KAFKA_PORT 
+    local result
+    for port in "${KAFKA_PORTS[@]}"; do
+        host="${KAFKA_HOST[$port]}"
+        nc -z -w 5 $host "$port" && result="true" && break
+    done
+    if [ "$result" == "true" ]; then
+        return 0
+    else
+        return 1
+    fi
 }
 
 until kafka_running; do
-    echo "Kafka is not available yet. Retrying in 10 seconds..."
-    sleep 10
+    echo "Kafka is not available yet on any of the specified ports. Retrying in 3 seconds..."
+    sleep 3
 done
 
-echo "Kafka is now available. Starting producer..."
+echo "Kafka is now available on at least one of the specified ports. Starting producer..."
 python producer.py
 echo "Done"
