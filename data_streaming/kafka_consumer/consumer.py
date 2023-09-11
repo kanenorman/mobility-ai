0b0b78b64e790401e29467ac4a7a80c83095f841
kanenorman <kanenorman1996@gmail.com>
2023-09-11 16:28:50 -0400

    Fix upsert issues between spark and PSQL
    Spark doesn't support INSERT IGNORE/REPLACE operations into a JDBC
    connection. As a work around, we create a staging table using the JDBC
    connection to preform the UPSERT using Postgres ON CONFLICT cause.

--- data_streaming/kafka_consumer/consumer.py
+++ data_streaming/kafka_consumer/consumer.py
@@ -1,5 +1,6 @@
 import sys
 
+import psycopg2
 import pyspark
 from config import configs
 from load_schedules import process_schedules_stream
@@ -7,57 +8,130 @@ from pyspark.sql import SparkSession
 from pyspark.sql.streaming import DataStreamWriter
 
 
+def _create_spark_session() -> pyspark.sql.SparkSession:
+    """
+    Create and configure a SparkSession.
+
+    Returns
+    -------
+    pyspark.sql.SparkSession
+        The configured SparkSession.
+    """
+    return (
+        SparkSession.builder.appName("MBTA Data Streaming")
+        .master("local[*]")
+        .getOrCreate()
+    )
+
+
+def _configure_spark_logging(spark: pyspark.sql.SparkSession) -> None:
+    """
+    Configure Spark logging level to ERROR.
+
+    Parameters
+    ----------
+    spark : pyspark.sql.SparkSession
+        The SparkSession to configure.
+    """
+    spark.sparkContext.setLogLevel("ERROR")
+
+
 def _write_to_database(batch: pyspark.sql.DataFrame, _: int) -> None:
     """
     Write batch data to a PostgreSQL database.
 
     Writes the given batch of data to a PostgreSQL database using JDBC.
+    Spark JDBC insert does not have native support for INSERT IGNORE/REPLACE type
+    operations. As a workaround, this function first inserts the batch into
+    a temporary (staging) table. The staging table is then UPSERTED into
+    the final production table USING A .
+
+    We are working under budget and time constraints for this project.
+    We consider this an acceptable solution given the circumstance.
 
     Parameters
     ----------
-    batch
+    batch : pyspark.sql.DataFrame
         The batch of data to be written to the database.
-    _
+    _ : int
         The ID of the current batch (unused in this function).
     """
+    # Database configuration
     host = configs.POSTGRES_HOST
     port = configs.POSTGRES_PORT
     database = configs.POSTGRES_DB
     url = f"jdbc:postgresql://{host}:{port}/{database}"
+    user = configs.POSTGRES_USER
+    password = configs.POSTGRES_PASSWORD
+    driver = configs.POSTGRES_DRIVER
+    table_name = configs.POSTGRES_TABLE
+    temp_table_name = f"temp_{table_name}"
 
-    print("writing to databse...")
-    (
-        batch.write.format("jdbc")
-        .option("driver", configs.POSTGRES_DRIVER)
-        .option("url", url)
-        .option("dbtable", configs.POSTGRES_TABLE)
-        .option("user", configs.POSTGRES_USER)
-        .option("password", configs.POSTGRES_PASSWORD)
-        .mode("append")
-        .option("saveMode", "ignore")
-        .save()
+    # Create a JDBC connection
+    connection = psycopg2.connect(
+        **{"host": host, "database": database, "user": user, "password": password}
     )
+    cursor = connection.cursor()
+
+    try:
+        # Create the temporary table matching the schema of the production table
+        cursor.execute(
+            f"""
+            CREATE TEMPORARY TABLE IF NOT EXISTS {temp_table_name}
+            (LIKE {table_name} INCLUDING CONSTRAINTS);
+            """
+        )
+
+        # Write data from 'batch' into the temporary table
+        (
+            batch.write.format("jdbc")
+            .option("driver", driver)
+            .option("url", url)
+            .option("dbtable", temp_table_name)
+            .option("user", user)
+            .option("password", password)
+            .mode("append")
+            .save()
+        )
+
+        # Perform upsert from temporary table to production
+        cursor.execute(
+            f"""
+            INSERT INTO {table_name}
+            SELECT * FROM {temp_table_name}
+            ON CONFLICT (id) DO NOTHING;
+            """
+        )
+
+        # Remove temporary table
+        cursor.execute(f"DROP TABLE IF EXISTS {temp_table_name};")
+        connection.commit()
+        print("wrote to database")
+    finally:
+        connection.close()
+        cursor.close()
 
 
-def main() -> None:
+def start_streaming_job() -> None:
     """
     Start the Spark streaming job.
 
     Sets up the Spark session, reads data from Kafka,
     processes it, and writes to a PostgreSQL database.
     """
-    spark = (
-        SparkSession.builder.appName("MBTA Data Streaming")
-        .master("local[*]")
-        .getOrCreate()
-    )
-    spark.sparkContext.setLogLevel("ERROR")
-
+    # Kafka configuration
     kafka_topic = configs.SCHEDULES_INPUT_TOPIC
     server_1 = f"{configs.KAFKA_HOST1}:{configs.KAFKA_PORT1}"
     server_2 = f"{configs.KAFKA_HOST2}:{configs.KAFKA_PORT2}"
     server_3 = f"{configs.KAFKA_HOST3}:{configs.KAFKA_PORT3}"
 
+    # Create a Spark session
+    spark = _create_spark_session()
+
+    # Configure Spark logging
+    _configure_spark_logging(spark)
+
+    # Read data from Kafka
     kafka_stream = (
         spark.readStream.format("kafka")
         .option("kafka.bootstrap.servers", f"{server_1},{server_2},{server_3}")
@@ -65,9 +139,12 @@ def main() -> None:
         .load()
     )
 
-    print("stream is read...")
+    print("Stream is read...")
+
+    # Process Kafka data
     processed_df = process_schedules_stream(kafka_stream)
 
+    # Define the Kafka query
     query_kafka: DataStreamWriter = (
         processed_df.writeStream.trigger(processingTime="10 seconds")
         .outputMode("update")
@@ -77,8 +154,9 @@ def main() -> None:
 
     query_kafka.awaitTermination()
 
+    # Stop the Spark session
     spark.stop()
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    sys.exit(start_streaming_job())
