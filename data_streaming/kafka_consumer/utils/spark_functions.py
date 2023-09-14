debfdc30404c996561ec540c47e0ff65964b86f3
kanenorman <kanenorman1996@gmail.com>
2023-09-14 11:45:40 -0400

    Finished Pyspark to PSQL upserts
    Initial version of spark to postgres upsert code snippet. Pyspark does
    not have native support for handling upserts on JDBC inserts. As a quick
    workaround, we are using a separate JDBC connector.

--- data_streaming/kafka_consumer/utils/spark_functions.py
+++ data_streaming/kafka_consumer/utils/spark_functions.py
@@ -9,22 +9,40 @@ from .config import configs
 
 
 def _get_postgres_connection():
-    host = configs.POSTGRES_HOST
-    database = configs.POSTGRES_DB
-    user = configs.POSTGRES_USER
-    password = configs.POSTGRES_PASSWORD
+    """
+    Create and return a Postgres database connection using the configurations.
 
+    Returns
+    -------
+    psycopg2.extensions.connection
+        The PostgreSQL database connection.
+    """
     return psycopg2.connect(
-        **{
-            "host": host,
-            "database": database,
-            "user": user,
-            "password": password,
-        }
+        host=configs.POSTGRES_HOST,
+        database=configs.POSTGRES_DB,
+        user=configs.POSTGRES_USER,
+        password=configs.POSTGRES_PASSWORD,
     )
 
 
 def _build_upsert_query(table_name: str, unique_key: str, columns: List[str]):
+    """
+    Build and return the upsert (INSERT ON CONFLICT) query for PostgreSQL.
+
+    Parameters
+    ----------
+    table_name : str
+        The name of the target table.
+    unique_key : str
+        The unique key for conflict resolution.
+    columns : List[str]
+        The list of column names to insert.
+
+    Returns
+    -------
+    str
+        The upsert query.
+    """
     column_names = ",".join(columns)
 
     return f"""INSERT INTO {table_name} ({column_names}) VALUES %s
@@ -32,12 +50,27 @@ def _build_upsert_query(table_name: str, unique_key: str, columns: List[str]):
 
 
 def _preform_upsert(batch, query, batch_size):
+    """
+    Execute the upsert operation on a batch of records in PostgreSQL.
+
+    Parameters
+    ----------
+    batch : Iterable
+        The batch of records to upsert.
+    query : str
+        The upsert query.
+    batch_size : int
+        The batch size for upserting records.
+
+    Returns
+    -------
+    None
+    """
     with _get_postgres_connection() as connection:
         with connection.cursor() as cursor:
             cursor = connection.cursor()
             execute_values(cur=cursor, sql=query, argslist=batch, page_size=batch_size)
             connection.commit()
-            print("wrote to databse")
 
 
 def _prepare_upsert(
@@ -45,6 +78,22 @@ def _prepare_upsert(
     query: str,
     batch_size: int = 1000,
 ):
+    """
+    Prepare and execute upsert operations on a partition of data.
+
+    Parameters
+    ----------
+    batch_partition : Iterable
+        The partition of data to upsert.
+    query : str
+        The upsert query.
+    batch_size : int, optional
+        The batch size for upserting records, by default 1000.
+
+    Returns
+    -------
+    None
+    """
     counter = 0
     batch = []
 
@@ -59,6 +108,8 @@ def _prepare_upsert(
     if len(batch) > 0:
         _preform_upsert(batch, query, batch_size)
 
+    yield None
+
 
 def write_to_database(
     batch: pyspark.sql.DataFrame,
@@ -67,6 +118,26 @@ def write_to_database(
     unique_key: str,
     parallelism: int = 1,
 ) -> None:
+    """
+    Write a batch of data to a PostgreSQL database using upsert (INSERT ON CONFLICT).
+
+    Parameters
+    ----------
+    batch : pyspark.sql.DataFrame
+        The batch of data to write.
+    epoch_id : int
+        The current epoch ID.
+    table_name : str
+        The name of the target table.
+    unique_key : str
+        The unique key for conflict resolution.
+    parallelism : int, optional
+        The degree of parallelism for writing data, by default 1.
+
+    Returns
+    -------
+    None
+    """
     upsert_query = _build_upsert_query(
         columns=batch.schema.names, table_name=table_name, unique_key=unique_key
     )
@@ -77,7 +148,7 @@ def write_to_database(
             query=upsert_query,
             batch_size=1000,
         )
-    )
+    ).collect()
 
 
 def create_spark_session() -> pyspark.sql.SparkSession:
