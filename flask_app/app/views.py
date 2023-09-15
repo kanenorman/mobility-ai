307cbd087ef2d8737682411572024240ee041200
kanenorman <kanenorman1996@gmail.com>
2023-09-14 22:00:24 -0400

    Render 10 records to homepage
    Render 10 records just to start

--- flask_app/app/views.py
+++ flask_app/app/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 
+from .extensions import database as db
 from .models import Schedule
 
 main = Blueprint("main", __name__)
@@ -8,5 +9,5 @@ main = Blueprint("main", __name__)
 @main.route("/")
 def index():
     """Index route for homepage."""
-    schedule_data = Schedule.query.all()
+    schedule_data = db.session.query(Schedule).limit(10).all()
     return render_template("index.html", data=schedule_data)
