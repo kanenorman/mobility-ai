74632dbf329801294b176b5cbbd6e7a83d62e253
kanenorman <kanenorman1996@gmail.com>
2023-09-26 21:16:54 -0400

    Fix ORM

--- flask_app/app/models/alert.py
+++ flask_app/app/models/alert.py
@@ -1,111 +1,73 @@
+from dataclasses import dataclass
+
 from ..extensions import database as db
 
 
+@dataclass
 class Alert(db.Model):
+    """
+    Alert data model.
+
+    Parameters
+    ----------
+    event : str
+        Event identifier.
+    id : str
+        Unique identifier for the event data.
+    active_period : str
+        Active period information.
+    banner : str
+        Event banner information.
+    cause : str
+        Event cause information.
+    created_at : datetime
+        The timestamp when the event data was created.
+    description : str
+        Event description.
+    effect : str
+        Event effect information.
+    header : str
+        Event header information.
+    image : str
+        Event image information.
+    image_alternative_text : str
+        Alternative text for the event image.
+    informed_entity : str
+        Informed entity information.
+    lifecycle : str
+        Event lifecycle information.
+    service_effect : str
+        Service effect of the event.
+    severity : str
+        Event severity.
+    short_header : str
+        Short header for the event.
+    timeframe : str
+        Event timeframe information.
+    updated_at : datetime
+        The timestamp when the event data was last updated.
+    type : str
+        Event type.
+    """
+
     __tablename__ = "alert"
 
-    event = db.Column(db.String(255), nullable=False)
-    id = db.Column(db.String(255), primary_key=True, nullable=False)
-    active_period = db.Column(db.String(255), nullable=True)
-    banner = db.Column(db.String(255), nullable=True)
-    cause = db.Column(db.String(255), nullable=True)
-    created_at = db.Column(db.TIMESTAMP, nullable=True)
-    description = db.Column(db.String(255), nullable=True)
-    effect = db.Column(db.String(255), nullable=True)
-    header = db.Column(db.String(255), nullable=True)
-    image = db.Column(db.String(255), nullable=True)
-    image_alternative_text = db.Column(db.String(255), nullable=True)
-    informed_entity = db.Column(db.String(255), nullable=True)
-    lifecycle = db.Column(db.String(255), nullable=True)
-    service_effect = db.Column(db.String(255), nullable=True)
-    severity = db.Column(db.String(255), nullable=True)
-    short_header = db.Column(db.String(255), nullable=True)
-    timeframe = db.Column(db.String(255), nullable=True)
-    updated_at = db.Column(db.TIMESTAMP, nullable=True)
-    type = db.Column(db.String(255), nullable=True)
-
-    def __init__(
-        self,
-        event,
-        id,
-        active_period,
-        banner,
-        cause,
-        created_at,
-        description,
-        effect,
-        header,
-        image,
-        image_alternative_text,
-        informed_entity,
-        lifecycle,
-        service_effect,
-        severity,
-        short_header,
-        timeframe,
-        updated_at,
-        type,
-    ):
-        """
-        Alert data model.
-
-        Parameters
-        ----------
-        event : str
-            Event identifier.
-        id : str
-            Unique identifier for the event data.
-        active_period : str
-            Active period information.
-        banner : str
-            Event banner information.
-        cause : str
-            Event cause information.
-        created_at : datetime
-            The timestamp when the event data was created.
-        description : str
-            Event description.
-        effect : str
-            Event effect information.
-        header : str
-            Event header information.
-        image : str
-            Event image information.
-        image_alternative_text : str
-            Alternative text for the event image.
-        informed_entity : str
-            Informed entity information.
-        lifecycle : str
-            Event lifecycle information.
-        service_effect : str
-            Service effect of the event.
-        severity : str
-            Event severity.
-        short_header : str
-            Short header for the event.
-        timeframe : str
-            Event timeframe information.
-        updated_at : datetime
-            The timestamp when the event data was last updated.
-        type : str
-            Event type.
-        """
-        self.event = event
-        self.id = id
-        self.active_period = active_period
-        self.banner = banner
-        self.cause = cause
-        self.created_at = created_at
-        self.description = description
-        self.effect = effect
-        self.header = header
-        self.image = image
-        self.image_alternative_text = image_alternative_text
-        self.informed_entity = informed_entity
-        self.lifecycle = lifecycle
-        self.service_effect = service_effect
-        self.severity = severity
-        self.short_header = short_header
-        self.timeframe = timeframe
-        self.updated_at = updated_at
-        self.type = type
+    event: str = db.Column(db.String(255), nullable=False)
+    id: str = db.Column(db.String(255), primary_key=True, nullable=False)
+    active_period: str = db.Column(db.String(255), nullable=True)
+    banner: str = db.Column(db.String(255))
+    cause: str = db.Column(db.String(255))
+    created_at = db.Column(db.TIMESTAMP)
+    description: str = db.Column(db.String(255))
+    effect: str = db.Column(db.String(255))
+    header: str = db.Column(db.String(255))
+    image: str = db.Column(db.String(255))
+    image_alternative_text: str = db.Column(db.String(255))
+    informed_entity: str = db.Column(db.String(255))
+    lifecycle: str = db.Column(db.String(255))
+    service_effect: str = db.Column(db.String(255))
+    severity: str = db.Column(db.String(255))
+    short_header: str = db.Column(db.String(255))
+    timeframe: str = db.Column(db.String(255))
+    updated_at: str = db.Column(db.TIMESTAMP)
+    type: str = db.Column(db.String(255))
