5aeb3f70bb6af78a502537fea685901cc074b0c4
kanenorman <kanenorman1996@gmail.com>
2023-10-04 20:18:59 -0400

    Fix kafka consumer imports

--- data_streaming/kafka_consumer/utils/__init__.py
+++ data_streaming/kafka_consumer/utils/__init__.py
@@ -1,3 +1,6 @@
 from .config import configs
-from .spark_functions import (configure_spark_logging, create_spark_session,
-                              write_to_database)
+from .spark_functions import (
+    configure_spark_logging,
+    create_spark_session,
+    write_to_database,
+)
