3fd99df05640d6890bdefb54445792af9ebe8dd3
kanenorman <kanenorman1996@gmail.com>
2023-11-08 12:24:06 -0500

    Remove Apache Spark Consumer

--- data_streaming/kafka_consumer/schemas/__init__.py
+++
@@ -1,7 +0,0 @@
-from .alerts import parse_alerts_topic
-from .routes import parse_routes_topic
-from .schedules import parse_schedules_topic
-from .shapes import parse_shapes_topic
-from .stops import parse_stops_topic
-from .trips import parse_trips_topic
-from .vehicles import parse_vehicles_topic
--- data_streaming/kafka_consumer/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-from .config import configs
-from .spark_functions import (
-    configure_spark_logging,
-    create_spark_session,
-    write_to_database,
-)
