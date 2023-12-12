d3dde19ae04cce42a23ad5853d3bb87710cf9895
Philip Ndikum <philipndikum@fas.harvard.edu>
2023-12-12 08:37:04 -0500

    Updated setup & k8s deployment yaml

--- machine_learning_app/setup_environment.sh
+++ machine_learning_app/setup_environment.sh
@@ -1,29 +1,62 @@
 #!/bin/bash
 
-# Start Minikube with Docker driver
-echo "Starting Minikube with Docker driver..."
-minikube start --driver=docker
+# Function to check if Minikube is running
+check_minikube_status() {
+    minikube_status=$(minikube status --format "{{.Host}}")
+    if [ "$minikube_status" != "Running" ]; then
+        echo "Starting Minikube with Docker driver..."
+        minikube start --driver=docker
+    else
+        echo "Minikube is already running."
+    fi
+}
 
-# Direct the shell to use Minikube's Docker daemon
-echo "Setting up Minikube's Docker environment..."
-eval $(minikube docker-env)
+# Function to build the Docker image
+build_docker_image() {
+    echo "Building the Docker image..."
+    eval $(minikube docker-env)
+    docker build -t machine_learning_app_prediction:latest -f Dockerfile.prediction .
+}
 
-# Build the Docker image
-echo "Building the Docker image..."
-docker build -t machine_learning_app_prediction:latest -f Dockerfile.prediction .
+# Function to deploy the application
+deploy_application() {
+    echo "Deploying the application..."
+    kubectl apply -f prediction-deployment.yaml
+}
 
-# Deploy the application using Kubernetes
-echo "Deploying the application..."
-kubectl apply -f prediction-deployment.yaml
+# Function to wait for pods to be in the 'Running' state
+wait_for_pods() {
+    echo "Waiting for pods to be in the 'Running' state..."
+    local retries=0
+    local max_retries=12 # Waits for up to 1 minute (12 retries * 5 seconds)
+    while : ; do
+        pod_status=$(kubectl get pods -l app=machine-learning-prediction -o custom-columns=STATUS:.status.phase --no-headers | grep -v Running)
+        if [ -z "$pod_status" ]; then
+            echo "All pods are running."
+            break
+        else
+            if [ $retries -eq $max_retries ]; then
+                echo "Timeout waiting for pods to be running."
+                exit 1
+            fi
+            echo "Waiting for pod to be running..."
+            sleep 5
+            ((retries++))
+        fi
+    done
+}
 
-# Confirm the deployment
-echo "Checking the deployment status..."
-kubectl get pods
-kubectl get deployments
-kubectl get services
+# Function to fetch the application URL
+fetch_application_url() {
+    echo "Fetching the application URL..."
+    minikube service machine-learning-prediction-service --url
+}
 
-# Access the application (specific to Minikube with NodePort service)
-echo "Fetching the application URL..."
-minikube service machine-learning-prediction-service --url
+# Main script execution
+check_minikube_status
+build_docker_image
+deploy_application
+wait_for_pods
+fetch_application_url
 
 echo "Setup complete."
